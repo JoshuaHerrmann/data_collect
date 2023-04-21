253e49cb728510a0f862e9dc33ff84166e800a6d
Joshua Herrmann <joshua.herrmann1@web.de>
2023-04-21 04:52:17 +0200

    first push

---
+++ data_collect.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+// initialzie variables
+var firstname string
+var lastname string
+var adress string
+var house_number int
+var postcode int
+var email_adress string
+
+func main() {
+	getArgData()
+	debug := create_json()
+	//debug
+	fmt.Printf("%s", debug)
+
+}
+
+func getArgData() {
+	//set commandline arguments connected to the values
+	flag.StringVar(&firstname, "f", "false", "Enter you firstname(string)")
+	flag.StringVar(&lastname, "l", "false", "Enter you lastname(string)")
+	flag.StringVar(&adress, "a", "false", "Enter you adress(string)")
+	flag.IntVar(&house_number, "hn", 0, "Enter you house_number(int)")
+	flag.IntVar(&postcode, "pc", 0, "Enter you postcode(int)")
+	flag.StringVar(&email_adress, "e", "false", "Enter you email_adress(string)")
+	//parse the flag values
+	flag.Parse()
+}
+
+// create map with all user data
+func create_json() []byte { //[]byte debug line
+	//error checking for default values
+	if firstname == "flase" || lastname == "false" || adress == "false" || email_adress == "false" || house_number == 0 || postcode == 0 {
+		log.Fatal("missing data, use all required flags; -f -l -a -hn -pc -e. Type -help for explanation.")
+	}
+	userdata := map[string]interface{}{
+		"firstname":    firstname,
+		"lastname":     lastname,
+		"adress":       adress + fmt.Sprint(house_number),
+		"postcode":     postcode,
+		"email_adress": email_adress}
+	//create json without prefix and 1 space indent
+
+	jsonData, err := json.MarshalIndent(userdata, "", " ")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	write_json_file(jsonData)
+	//debug line
+	return jsonData
+}
+
+// creates a new .json file with the users first- and lastname as title and data from jsonData []byte object.
+func write_json_file(jsonData []byte) {
+	err := ioutil.WriteFile(fmt.Sprintf("%v%v.json", firstname, lastname), jsonData, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joshuaherrmann/data_collect
+
+go 1.20
