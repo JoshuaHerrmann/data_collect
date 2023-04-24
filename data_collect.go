c837ba48b4726fa5b289c5a0ef056c991b4c4e75
Joshua Herrmann <joshua.herrmann1@web.de>
2023-04-24 13:52:24 +0200

    updated path validation for create .json file

--- data_collect.go
+++ data_collect.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 // initialzie variables
@@ -15,6 +16,7 @@ var adress string
 var house_number int
 var postcode int
 var email_adress string
+var path string
 
 func main() {
 	getArgData()
@@ -32,6 +34,7 @@ func getArgData() {
 	flag.IntVar(&house_number, "hn", 0, "Enter you house_number(int)")
 	flag.IntVar(&postcode, "pc", 0, "Enter you postcode(int)")
 	flag.StringVar(&email_adress, "e", "false", "Enter you email_adress(string)")
+	flag.StringVar(&path, "path", "", "Enter the path where the .json file should be created. If not used it just returns a commandline string(string)")
 	//parse the flag values
 	flag.Parse()
 }
@@ -48,22 +51,31 @@ func create_json() []byte { //[]byte debug line
 		"adress":       adress + fmt.Sprint(house_number),
 		"postcode":     postcode,
 		"email_adress": email_adress}
-	//create json without prefix and 1 space indent
 
+	//create json without prefix and 1 space indent
 	jsonData, err := json.MarshalIndent(userdata, "", " ")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	write_json_file(jsonData)
+	if path != "" {
+		write_json_file(jsonData)
+	}
+
 	//debug line
 	return jsonData
 }
 
 // creates a new .json file with the users first- and lastname as title and data from jsonData []byte object.
 func write_json_file(jsonData []byte) {
-	err := ioutil.WriteFile(fmt.Sprintf("%v%v.json", firstname, lastname), jsonData, 0644)
+	//checks if the dir path is available, if not fatal error stop programm
+	_, error := os.ReadDir(path)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	err := ioutil.WriteFile(fmt.Sprintf("%v"+"%v%v.json", path, firstname, lastname), jsonData, 0644)
 
 	if err != nil {
 		log.Fatal(err)
